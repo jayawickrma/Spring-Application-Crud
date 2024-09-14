9022eb4d1f5927ba802e87c6dc0ade8907b9e370
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-09-14 23:52:12 +0530

    created pakages and some classes

--- src/main/java/com/example/demo2/Controller/CustomerController.java
+++ src/main/java/com/example/demo2/Controller/CustomerController.java
@@ -1,7 +1,9 @@
 package com.example.demo2.Controller;
 
 import com.example.demo2.DTO.IMPL.Customer;
+import com.example.demo2.Service.CustomerService;
 import com.example.demo2.Utill.AppUtill;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v2/customer")
 public class CustomerController {
+    @Autowired
+   private CustomerService service;
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
                  produces = MediaType.APPLICATION_JSON_VALUE)
     public Customer saveCustomer(Customer customerDto){
         customerDto.setCustomerID(AppUtill.generateCustomerId());
+        return service.saveCustomer(customerDto);
     }
 }
